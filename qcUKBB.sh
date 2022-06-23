55a626bb32305752ad6939e9ae27fe27b4654a36
Dariush Ghasemi Semeskandeh <dghasemisemeskandeh@gm.eurac.edu>
2022-06-23 22:27:28 +0200

    format conversion BGEN to plink .bed/.bim/.fam

---
+++ qcUKBB.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#genotype=~/projects/gwas/replicationAnalysis/formats_merge_conversion
+genotype=/scratch/UKBB/imputed-500k_V3
+
+#Convert BGEN to plink(bed/bim/fam) and simulatneously pruning SNPs
+
+for chr in 21  22; #{1..22};  
+	do
+		plink2  --bgen $genotype/ukb_imp_chr${chr}_v3.bgen  \
+			ref-first   \
+			--sample $genotype/ukb_imp_chr${chr}_v3.sample  \
+			--maf 0.05  \
+			--mac 10    \
+			--write-snplist \
+			--write-samples  \
+			--no-id-header \
+			--make-bed  \
+			--out $genotype/test/ukb_imp_chr${chr}_v3.pruned
+done
+
+#making list of the plink files to be merged
+ls $genotype/test/ukb_imp_chr*_v3.pruned.bed | sed 's/.bed//' > $genotype/test/myfilesnames.txt
+
+#merge plink files
+plink --merge-list $genotype/test/myfilesnames.txt --make-bed --out $genotype/test/chr21_22_Merged
+
+
+
