347bbd50a4ceae81bad1dc81352e980e29b45f74
DariushG3 <dariush.ghasemi@eurac.edu>
2022-10-19 18:44:26 +0200

    Running GWAS prediction step

--- GWAS_Regenie_step1.sh
+++ GWAS_Regenie_step1.sh
@@ -3,19 +3,20 @@
 Genotype=~/projects/UKBB_Ryo
 Phenotype=/home/rfujii/ukbb/00_data
 Output=~/projects/UKBB_Ryo/output
+#Genotype=/scratch/UKBB/imputed-500k_V3
 #Samples=/scratch/UKBB/imputed-500k_V3/ukb_imp_chr21_v3.sample
-#pheno_Pheno1_Pheno2.txt
-  #--lowmem \
-  #--lowmem-prefix ${Output}/tmpdir/regenie_tmp_preds \
-#${}
+#--keep ${Genotype}/remained.sample \
 
 echo "regenie \
   --step 1 \
-  --bgen ${Genotype}/concatedated_chr21_22_plink.pruned0.bgen \
-  --sample ${Genotype}/ukbb_chr21_22_plink_pruned0.sample3 \
+  --bed ${Genotype}/genotypedUKBB \
+  --sample ${Genotype}/genotypedUKBB.sample  \
   --phenoFile ${Phenotype}/pheno.txt \
   --covarFile ${Phenotype}/cov.txt \
   --phenoColList Pheno1,Pheno2 \
   --bsize "1000" \
-  --out ${Output}/step1_fit_UKBB_Pheno1_with_AGE_SEX_PCs "
+  --force-step1 \
+  --lowmem \
+  --lowmem-prefix ${Output}/tmpdir/regenie_tmp_preds \
+  --out ${Output}/step1_fit_UKBB_AllChrs_with_AGE_SEX_PCs "
 
